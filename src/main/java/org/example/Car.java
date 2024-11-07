957e674e83194c4db2fc0ecbc6b0e0ca12fd149c
Abdelrahman Mostafa <114619776+AbdooMatrix@users.noreply.github.com>
2024-11-07 18:16:24 +0200

    ..

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,33 +1,5 @@
 package org.example;
 
 public class Car {
-    private int id;
-    private int gate;
-    private int arrivalTime;
-    private int parkingDuration;
 
-    void setId (int id) {
-        this.id = id;
-    }
-    void setGate (int gate) {
-        this.gate = gate;
-    }
-    void setArrivalTime (int arrivalTime) {
-        this.arrivalTime = arrivalTime;
-    }
-    void setParkingDuration (int parkingDuration) {
-        this.parkingDuration = parkingDuration;
-    }
-    int getId () {
-        return id;
-    }
-    int getGate () {
-        return gate;
-    }
-    int getArrivalTime () {
-        return arrivalTime;
-    }
-    int getParkingDuration () {
-        return parkingDuration;
-    }
 }
--- src/main/java/org/example/Gate.java
+++ src/main/java/org/example/Gate.java
@@ -1,13 +1,6 @@
 package org.example;
 
 public class Gate {
-    private static int servedCars = 0;
 
-    int getServedCars() {
-        return servedCars;
-    }
-    void incrementServedCars() {
-        servedCars++;
-    }
 
 }
